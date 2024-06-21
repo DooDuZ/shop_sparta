8d0e3dd1df3615ac4ea2f29ed6657363de5dd484
DooDuZ <sin9158@naver.com>
2024-06-22 03:08:37 +0900

    feat: member controller/service/repository

---
+++ src/main/java/com/sparta/shop_sparta/controller/member/MemberControllerImpl.java
@@ -0,0 +1,31 @@
+package com.sparta.shop_sparta.controller.member;
+
+import com.sparta.shop_sparta.domain.dto.member.LoginResponseDTO;
+import com.sparta.shop_sparta.domain.dto.member.MemberDTO;
+
+public class MemberControllerImpl implements MemberController{
+    @Override
+    public MemberDTO createAccount(MemberDTO memberDTO) {
+        return null;
+    }
+
+    @Override
+    public LoginResponseDTO login(MemberDTO memberDTO) {
+        return null;
+    }
+
+    @Override
+    public void logout() {
+
+    }
+
+    @Override
+    public void updatePassword(String currentPassword, String newPassword) {
+
+    }
+
+    @Override
+    public void updatePhoneNumber(String PhoneNumber) {
+
+    }
+}
