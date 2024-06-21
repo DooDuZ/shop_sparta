5d4417c29d2cefa4690e12f83ed9d142fbd73805
DooDuZ <sin9158@naver.com>
2024-06-21 22:06:34 +0900

    feat: Enum 추가 / OrderResponseMessage
    주문 결과 메시지 Enum

---
+++ src/main/java/com/sparta/shop_sparta/domain/dto/constant/OrderResponseMessage.java
@@ -0,0 +1,18 @@
+package com.sparta.shop_sparta.domain.dto.constant;
+
+public enum OrderResponseMessage {
+    SUCCESS_ORDER("[주문 성공]"),
+    COMPLETE_ORDER("[주문 처리 완료] - 이미 처리된 주문입니다."),
+    FAIL_PAYMENT("[결제 실패] 결제 수단을 확인해주세요."),
+    SOLD_OUT_ITEM("[재고 소진] 남은 수량이 적습니다.");
+
+    private final String message;
+
+    OrderResponseMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
