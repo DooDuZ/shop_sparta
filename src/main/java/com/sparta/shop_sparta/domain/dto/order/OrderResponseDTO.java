dcb9d93f9cdeefe5401f5ce1124900cd33032dfd
DooDuZ <sin9158@naver.com>
2024-06-21 22:52:57 +0900

    feat: DTO 추가 / OrderResponseDTO
    ResponseDTO 추상 클래스 상속

--- src/main/java/com/sparta/shop_sparta/domain/dto/order/OrderResponseDTO.java
+++ src/main/java/com/sparta/shop_sparta/domain/dto/order/OrderResponseDTO.java
@@ -1,15 +1,14 @@
 package com.sparta.shop_sparta.domain.dto.order;
 
+import com.sparta.shop_sparta.domain.dto.ResponseDTO;
 import com.sparta.shop_sparta.domain.dto.constant.OrderResponseMessage;
 import lombok.Getter;
 
 @Getter
-public class OrderResponseDTO {
-    private Boolean result;
-    private String message;
-
-    public OrderResponseDTO(Boolean result, OrderResponseMessage message){
-        this.result = result;
-        this.message = message.getMessage();
+public class OrderResponseDTO extends ResponseDTO {
+    OrderDTO orderDTO;
+    public OrderResponseDTO(Boolean result, OrderResponseMessage message, OrderDTO orderDTO){
+        super(result, message.getMessage());
+        this.orderDTO = orderDTO;
     }
 }
