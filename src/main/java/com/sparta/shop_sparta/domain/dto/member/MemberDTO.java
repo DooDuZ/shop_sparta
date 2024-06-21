3c0efd14a361d3abe301269b247d300a8ad2fc2d
DooDuZ <sin9158@naver.com>
2024-06-22 03:04:56 +0900

    feat: memberDTO setter 추가

--- src/main/java/com/sparta/shop_sparta/domain/dto/member/MemberDTO.java
+++ src/main/java/com/sparta/shop_sparta/domain/dto/member/MemberDTO.java
@@ -25,4 +25,12 @@ public class MemberDTO {
         return MemberEntity.builder().memberId(this.memberId).loginId(this.loginId).password(this.password)
                 .memberName(this.memberName).phoneNumber(this.phoneNumber).email(this.email).role(this.role).build();
     }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
