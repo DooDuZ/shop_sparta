7aac6f6761a877cb753b6070b3d2761233d56b62
DooDuZ <sin9158@naver.com>
2024-06-22 20:31:47 +0900

    test: 복호화 값 일치 테스트

--- src/test/java/com/sparta/shop_sparta/securitytest/EncryptionTest.java
+++ src/test/java/com/sparta/shop_sparta/securitytest/EncryptionTest.java
@@ -4,7 +4,8 @@ import com.sparta.shop_sparta.config.security.encoder.SaltGenerator;
 import com.sparta.shop_sparta.config.security.encoder.UserInformationEncoder;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -17,12 +18,11 @@ public class EncryptionTest {
     @Autowired
     UserInformationEncoder userInformationEncoder;
 
-    @Test
-    @DisplayName("암호화 후 복호화 값이 동일한지 확인합니다.")
-    void encodeTest() {
+    @ParameterizedTest
+    @DisplayName("데이터 암호화 후 복호화 값이 동일한지 확인합니다.")
+    @ValueSource(strings = {"!@#!$ASd", "zxcvd23!@#", "test12!@", "1234", "name", "addr", "경기도 안산시 단원구 고잔2길 9 하이베라스 541호"})
+    void encodeTest(String password) {
         String salt = saltGenerator.generateSalt();
-
-        String password = "1234";
 
         String encodedPassword = userInformationEncoder.encrypt(password, salt);
         String decodedPassword = userInformationEncoder.decrypt(encodedPassword, salt);
