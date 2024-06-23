fbc49649605f10ac67847668c2d3f9bb7a88dff8
DooDuZ <sin9158@naver.com>
2024-06-24 01:12:12 +0900

    feat: 회원 주소 저장 구현

--- src/main/java/com/sparta/shop_sparta/service/member/AddrServiceImpl.java
+++ src/main/java/com/sparta/shop_sparta/service/member/AddrServiceImpl.java
@@ -1,18 +1,37 @@
 package com.sparta.shop_sparta.service.member;
 
+import com.sparta.shop_sparta.domain.dto.member.AddrDto;
 import com.sparta.shop_sparta.domain.entity.member.AddrEntity;
+import com.sparta.shop_sparta.domain.entity.member.MemberEntity;
+import com.sparta.shop_sparta.repository.AddrRepository;
+import com.sparta.shop_sparta.util.encoder.SaltGenerator;
+import com.sparta.shop_sparta.util.encoder.UserInformationEncoder;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AddrServiceImpl implements AddrService{
+    private final SaltGenerator saltGenerator;
+    private final UserInformationEncoder userInformationEncoder;
+    private final AddrRepository addrRepository;
     @Override
-    public void addAddr(Long memberId, String addr, String addrDetail) {
-        AddrEntity addrEntity = AddrEntity.builder().build();
+    public void addAddr(MemberEntity memberEntity, String addr, String addrDetail) {
+        String salt = saltGenerator.generateSalt();
+
+        AddrEntity addrEntity = AddrEntity.builder()
+                .memberEntity(memberEntity)
+                .addr(userInformationEncoder.encrypt(addr, salt))
+                .addr_detail(userInformationEncoder.encrypt(addrDetail, salt))
+                .build();
+
+        addrRepository.save(addrEntity);
     }
 
     @Override
     public void removeAddr(Long addrId) {
-
+        addrRepository.deleteById(addrId);
     }
 
     @Override
@@ -21,12 +40,12 @@ public class AddrServiceImpl implements AddrService{
     }
 
     @Override
-    public void getAddr(Long addrId) {
-
+    public AddrDto getAddr(Long addrId) {
+        return null;
     }
 
     @Override
-    public void getAddrList(Long memberId) {
-
+    public List<AddrDto> getAddrList(Long memberId) {
+        return null;
     }
 }
