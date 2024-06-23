f8741b4b7b12658aabd24a92f6d41f7f088037ba
DooDuZ <sin9158@naver.com>
2024-06-23 19:38:08 +0900

    feat: .env test

---
+++ src/test/java/com/sparta/shop_sparta/mailsender/EmailSenderTest.java
@@ -0,0 +1,16 @@
+package com.sparta.shop_sparta.mailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailSenderTest {
+
+    @Value("${REQUEST_URL}")
+    String url;
+    @Test
+    void test(){
+        System.out.println(url);
+    }
+}
