6717a1764d7bd3c7c9eb6ae62b44fde1df8e4432
DooDuZ <sin9158@naver.com>
2024-06-23 23:52:33 +0900

    feat: MemberRole Enum grade추가

--- src/main/java/com/sparta/shop_sparta/constant/member/MemberRole.java
+++ src/main/java/com/sparta/shop_sparta/constant/member/MemberRole.java
@@ -1,6 +1,17 @@
 package com.sparta.shop_sparta.constant.member;
 
+import lombok.Getter;
+
+@Getter
 public enum MemberRole {
-    ADMIN,
-    BASIC
+    ADMIN("ADMIN"),
+    BASIC("BASIC"),
+    UNVERIFIED("UNVERIFIED"),
+    ;
+
+    private final String grade;
+
+    MemberRole(String grade){
+        this.grade = grade;
+    }
 }
