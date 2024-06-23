96a85bae3a71ad85dbb74e24340229a60ce3f636
DooDuZ <sin9158@naver.com>
2024-06-23 15:02:53 +0900

    chore: PasswordValidator 메서드 이름 변경
    메서드에 password 명시

--- src/main/java/com/sparta/shop_sparta/validator/member/PasswordValidator.java
+++ src/main/java/com/sparta/shop_sparta/validator/member/PasswordValidator.java
@@ -1,6 +1,6 @@
 package com.sparta.shop_sparta.validator.member;
 
 public interface PasswordValidator {
-    Boolean checkPattern(String password);
-    Boolean checkSame(String password, String newPassword);
+    Boolean checkPasswordPattern(String password);
+    Boolean checkSamePassword(String password, String newPassword);
 }
--- src/main/java/com/sparta/shop_sparta/validator/member/PasswordValidatorImpl.java
+++ src/main/java/com/sparta/shop_sparta/validator/member/PasswordValidatorImpl.java
@@ -5,18 +5,18 @@ import java.util.regex.Pattern;
 
 public class PasswordValidatorImpl implements PasswordValidator{
 
-    private final String regex = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=])(?=.*[0-9]).{10,16}$";
+    private final String passwordRegex = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=])(?=.*[0-9]).{10,16}$";
 
-    private final Pattern pattern = Pattern.compile(regex);
+    private final Pattern passwordPattern = Pattern.compile(passwordRegex);
 
     @Override
-    public Boolean checkPattern(String password) {
-        Matcher matcher = pattern.matcher(password);
+    public Boolean checkPasswordPattern(String password) {
+        Matcher matcher = passwordPattern.matcher(password);
         return matcher.matches();
     }
 
     @Override
-    public Boolean checkSame(String password, String newPassword) {
+    public Boolean checkSamePassword(String password, String newPassword) {
         return password.equals(newPassword);
     }
 }
