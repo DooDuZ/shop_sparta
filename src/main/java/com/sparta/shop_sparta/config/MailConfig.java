ea3dccec876f2046d14da39d9bd489039ef129e1
DooDuZ <sin9158@naver.com>
2024-06-24 04:19:53 +0900

    test: GitActions test

--- src/main/java/com/sparta/shop_sparta/config/MailConfig.java
+++ src/main/java/com/sparta/shop_sparta/config/MailConfig.java
@@ -12,8 +12,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
-@PropertySource("classpath:/application.yml")
-@ConfigurationProperties(prefix = "spring.mail")
+//@PropertySource("classpath:/application.yml")
+//@ConfigurationProperties(prefix = "spring.mail")
 @Getter
 @Setter
 public class MailConfig {
@@ -24,10 +24,15 @@ public class MailConfig {
     public String from;
     public String DOMAIN_NAME = "두두지월드";
 
-    private String username;
-    private String password;
-    private int port;
-    private Properties properties;
+    //private String username;
+    //private String password;
+    //private int port;
+    //private Properties properties;
+
+    private String username = "test";
+    private String password = "test";
+    private int port = 1;
+    private Properties properties = null;
 
     @Bean
     JavaMailSender javaMailSender(){
