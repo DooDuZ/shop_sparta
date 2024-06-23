5bb8021dd078fc0ff97c8ede4aed3d52fe035043
DooDuZ <sin9158@naver.com>
2024-06-24 00:00:42 +0900

    feat: MemberController 가입 인증 기능 추가
    verifySignup 메서드 추가

--- src/main/java/com/sparta/shop_sparta/controller/member/MemberControllerImpl.java
+++ src/main/java/com/sparta/shop_sparta/controller/member/MemberControllerImpl.java
@@ -2,18 +2,33 @@ package com.sparta.shop_sparta.controller.member;
 
 import com.sparta.shop_sparta.domain.dto.member.LoginResponseDto;
 import com.sparta.shop_sparta.domain.dto.member.MemberDto;
+import com.sparta.shop_sparta.service.member.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberControllerImpl implements MemberController{
+
+    private final MemberService memberServiceImpl;
+
     @Override
     @PostMapping("/")
     public MemberDto createAccount(@RequestBody MemberDto memberDTO) {
-        return memberDTO;
+        return memberServiceImpl.createAccount(memberDTO);
+    }
+
+    @Override
+    @GetMapping("/verification")
+    public Boolean verifySignup(@RequestParam("memberId") Long memberId, @RequestParam("verificationCode") String verificationCode) {
+        System.out.println("test");
+        return memberServiceImpl.verifySignup(memberId, verificationCode);
     }
 
     @Override
