bfa120baf88f2d17eb7bfe568c4c58faa8feacf5
DooDuZ <sin9158@naver.com>
2024-06-26 04:41:59 +0900

    feat: JwtTokenProvider
    getRole, getExpireDate 추가
    access / refresh 발급 로직 분리

--- src/main/java/com/sparta/shop_sparta/config/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/sparta/shop_sparta/config/security/jwt/JwtTokenProvider.java
@@ -5,12 +5,12 @@ import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -28,25 +28,43 @@ public class JwtTokenProvider {
     @Value("${JWT_SECRET}")
     private String secretKey;
     // 유효기간 15분
-    private final long tokenValidMillisecond = 1000L * 60 * 15;
+    private final long accessTokenValidMillisecond = 1000L * 60 * 15;
+
+    private final long refreshTokenValidMillisecond = 1000L * 60 * 60 * 24 * 7;
 
     @PostConstruct
     protected void init(){
         secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes(StandardCharsets.UTF_8));
     }
 
-    public String createToken(String username, String role){
+    public String createAccessToken(String username, String role){
         Map<String, Object> claims = new HashMap<>();
         claims.put("sub", username);
         claims.put("role", role);
-
 
         Date now = new Date();
 
         String token = Jwts.builder()
                 .claims(claims)
                 .issuedAt(now)
-                .expiration(new Date(now.getTime() + tokenValidMillisecond))
+                .expiration(new Date(now.getTime() + accessTokenValidMillisecond))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+
+        return token;
+    }
+
+    public String createRefreshToken(String username, String role){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("sub", username);
+        claims.put("role", role);
+
+        Date now = new Date();
+
+        String token = Jwts.builder()
+                .claims(claims)
+                .issuedAt(now)
+                .expiration(new Date(now.getTime() + refreshTokenValidMillisecond))
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
 
@@ -62,17 +80,42 @@ public class JwtTokenProvider {
         return Jwts.parser().setSigningKey(secretKey).build().parseSignedClaims(token).getPayload().getSubject();
     }
 
-    public String resolveToken(HttpServletRequest request){
-        return request.getHeader("X-AUTH-TOKEN");
+    public String getRole(String token){
+        return Jwts.parser().setSigningKey(secretKey).build().parseSignedClaims(token).getPayload().get("role").toString();
     }
 
-    public boolean validateToken(String token) {
-        try{
-            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).build().parseSignedClaims(token);
+    public Date getExpireDate(String token){
+        return Jwts.parser().setSigningKey(secretKey).build().parseSignedClaims(token).getPayload().getExpiration();
+    }
 
-            return !claims.getPayload().getExpiration().before(new Date());
+    public String resolveToken(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        String token = "";
+
+        if(cookies != null){
+            for(Cookie cookie : cookies){
+                if(cookie.getName().equals("access_token")){
+                    token = cookie.getValue();
+                }
+            }
+        }
+        //System.out.println(bearerToken);
+        if (!token.equals("")) {
+            //System.out.println(bearerToken.substring(7));
+            return token;
+        }
+        return null;
+    }
+
+    public Boolean isExpired(Jws<Claims> claims){
+        return claims.getPayload().getExpiration().before(new Date());
+    }
+
+    public Jws<Claims> validateToken(String token) {
+        try{
+            return Jwts.parser().setSigningKey(secretKey).build().parseSignedClaims(token);
         }catch (Exception e){
-            return false;
+            return null;
         }
     }
 }
