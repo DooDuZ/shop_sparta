ccf6312e273d21c48c34ea5343f729f0b915a96a
DooDuZ <sin9158@naver.com>
2024-06-26 01:40:30 +0900

    feat: JwtRedisRepository 추가
    리프레시 토큰 저장하는 리포지토리 추가

---
+++ src/main/java/com/sparta/shop_sparta/repository/memoryRepository/JwtRedisRepository.java
@@ -0,0 +1,38 @@
+package com.sparta.shop_sparta.repository.memoryRepository;
+
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class JwtRedisRepository implements RedisRepository<String, Object> {
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final String prefix = "refresh-code";
+
+    @Override
+    public void saveWithDuration(String key, Object value, Integer minute) {
+        Duration timeout = Duration.ofMinutes(minute);
+        redisTemplate.opsForValue().set(addPrefix(key), value,timeout);
+    }
+
+    @Override
+    public void save(String key, Object value) {
+        redisTemplate.opsForValue().set(addPrefix(key), value);
+    }
+
+    @Override
+    public Object find(String key) {
+        return redisTemplate.opsForValue().get(addPrefix(key));
+    }
+
+    @Override
+    public void delete(String key) {
+        redisTemplate.delete(addPrefix(key));
+    }
+
+    private String addPrefix(String key){
+        return prefix + key;
+    }
+}
