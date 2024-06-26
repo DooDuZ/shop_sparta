dea86b1a4b24578aa0b6b0ff601207c69a17f6a5
DooDuZ <sin9158@naver.com>
2024-06-27 02:12:22 +0900

    fix: 요청 url별 권한 정보 수정

--- src/main/java/com/sparta/shop_sparta/config/security/SecurityConfig.java
+++ src/main/java/com/sparta/shop_sparta/config/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.sparta.shop_sparta.config.security.jwt.LoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,26 +19,28 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final LoginSuccessHandler loginSuccessHandler;
 
-    // 개발 초기 단계 모든 접근 허용
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.
                 authorizeHttpRequests( authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/login","/member/", "/member/verification","/auth/token" ).permitAll()
+                                .requestMatchers("/login", "/member/verification","/auth/token" ).permitAll()
+                                .requestMatchers(HttpMethod.POST, "/member").permitAll()
+                                .requestMatchers("/member/**").authenticated()
                                 .anyRequest().hasAnyRole("BASIC", "ADMIN")
                 )
                 .sessionManagement( sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                // UsernamePasswordAuthenticationFilter의 인증이 끝난 뒤 successHandler 실행
                 .formLogin(formLogin ->
                         formLogin
                                 .successHandler(loginSuccessHandler)
                                 .permitAll()
-                )
 
+                )
+                //.formLogin( formLogin -> formLogin.disable())
                 .csrf(csrf -> csrf.disable());
 
         return http.build();
