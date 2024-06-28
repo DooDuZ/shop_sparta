4d88cbb675b4c1855c1a9d3044ddc5800fc3c04e
DooDuZ <sin9158@naver.com>
2024-06-28 12:23:44 +0900

    fix: AuthenticationManager 빈 등록 방식 변경
    http.build() -> AuthenticationConfiguration 에서 가져오기

--- src/main/java/com/sparta/shop_sparta/config/security/SecurityConfig.java
+++ src/main/java/com/sparta/shop_sparta/config/security/SecurityConfig.java
@@ -5,11 +5,14 @@ import com.sparta.shop_sparta.config.security.jwt.LoginAuthenticationFilter;
 import com.sparta.shop_sparta.config.security.jwt.LoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -25,8 +28,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final LoginSuccessHandler loginSuccessHandler;
-    private final PasswordEncoder passwordEncoder;
-    private final UserDetailsService userDetailsService;
+    private final AuthenticationConfiguration authenticationConfiguration;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -41,27 +43,31 @@ public class SecurityConfig {
                 .sessionManagement( sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .addFilterBefore(loginAuthenticationFilter(authenticationManager(http)), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(loginAuthenticationFilter(authenticationConfiguration.getAuthenticationManager()), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .formLogin(formLogin ->
-                        formLogin.disable()
-                                //.successHandler(loginSuccessHandler)
-                                //.permitAll()
-
-                )
-                //.formLogin( formLogin -> formLogin.disable())
+                .formLogin( formLogin -> formLogin.disable())
                 .csrf(csrf -> csrf.disable());
 
         return http.build();
+        //return null;
     }
-
+/*
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        System.out.println("SecurityConfig.authenticationManager");
+        System.out.println("http.hashCode() = " + http.hashCode());
         AuthenticationManagerBuilder authenticationManagerBuilder =
                 http.getSharedObject(AuthenticationManagerBuilder.class);
         authenticationManagerBuilder.userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder);
         return authenticationManagerBuilder.build();
+        //return http.getSharedObject(AuthenticationManager.class);
+    }
+ */
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
